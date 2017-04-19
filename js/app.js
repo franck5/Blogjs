682d518130e31af2cdc7f308ca2e2577daa3b6a3
dubech <franck.dubech@hotmail.com>
2017-04-19 16:40:22 +0200

    Maj du blog

--- js/app.js
+++ js/app.js
@@ -1,60 +1,90 @@
-$(document).ready(function(){
-	var Recup = [];
+
+var titre = ""
+var text = ""
+
+$("#titre").keyup(function(){
+	titre = $("#titre").val();
+
+});	
+
+$("#text").keyup(function(){
+	text = $("#text").val();
+
+});	
+
+$("#envoyer").click(function(){
+
+	var valeur = {"titre" : titre, "text" : text}
+	$("input").val("");
+	$("textarea").val("");
 	
-
-	$("#valider").click(function(){
-		var Text = $("#text").val();
-		var Titre = $("#titre").val();
-		var blog = {"title":Titre, "texte":Text}
-		Recup.push(blog);
-		console.log(Recup);
-		Save();
-
-	})
-
-	$("#Charger").click(function(){
-		$("Recuptext").html(Recup);
-		Charger();
-	})
-
-	
-
-
-	function Save(){
-		$.ajax({
-			url:'http://192.168.1.50/json-db',
-			data: {
-				task: 'set',
-				key: 'BlogFranck',
-				value: JSON.stringify(Recup)
-
-			}
-		});
-	};
-	
-	function Charger(){
-		$.ajax({
-			url:'http://192.168.1.50/json-db',
-			data: {
-			task: 'get',
-			key: 'BlogFranck'
-
-			},
-			success: function(data){
-				console.log(data);
-				//$("#Recuptext").append(Recup);
-			}
-		});
-	}
-
-	function Delete(){
-		$.ajax({
-			url:'http://192.168.1.50/json-db',
-			data: {
-			task: 'delete',
-			key: 'BlogFranck'
-			}
-		});
-
-	}
+    $.ajax({
+        
+        url:'http://192.168.1.50/json-db',
+        
+        data: {
+            
+            task: 'set',
+            
+            key: 'FranckBlog2',
+            
+            value: JSON.stringify(valeur),
+        }
+    });
+    
 });
+
+
+function Ajouter(table){
+ 	for (var i = 0; i < table.length; i++) {
+ 		$("#title").append('<a><div><li>'+table[i].titre+'</div></li></a>');
+ 		$("#article").append('<div>'+table[i].text+'</div>')
+ 	}
+}
+
+function Addadmin(tableau){
+for (var i = 0; i < tableau.length; i++) {
+ 		$("#suptitle").append('<a><div><li>'+tableau[i].titre+'</div></li></a>');
+ 		$("#suparticle").append('<div>'+tableau[i].text+'</div>'+ '<button id="sup">sup</button>')
+ 	}
+ }
+
+function affichage(){
+	$.ajax({ 
+		url:'http://192.168.1.50/json-db', 
+		data: { 
+			task: 'get',
+			key: 'FranckBlog2', 
+		} 
+	})
+	.done(function(data){
+
+		valeur=JSON.parse(data);
+		console.log(valeur);
+		Ajouter(valeur);
+		Addadmin(valeur)
+	});	
+
+}
+
+affichage();
+
+	$("#supprimer").click(function(){
+		
+		$.ajax({
+    		url: 'http://192.168.1.50/json-db',
+    		data: {
+    			task: 'delete',
+    			key: 'FranckBlog2',
+   			}
+		});
+	});
+
+	$("#text").keyup(function(){
+	
+	var converter = new showdown.Converter(),
+    	text      = $('#text').val(),
+    	html      = converter.makeHtml(text);
+	
+		$("#texthtml").html(html);
+	});
